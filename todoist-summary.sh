3149c536a87e0f6ec51951f93629e7bea8548304
Quang-Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-11-19 09:20:03 +0700

    Add action button for todoist

--- todoist-summary.sh
+++ todoist-summary.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+BUTTON=${button:-}
+
+if [ "x${BUTTON}" == "x1" ]; then
+    ACTION=$(xrescat i3xrocks.action.todoist "xdg-open https://todoist.com/app/#/today")
+    /usr/bin/i3-msg -q exec "$ACTION"
+fi
+
 # Setup https://github.com/sachaos/todoist
 # Get total task today, and print the first task in the list
 todoist sync && todoist list --filter '(overdue | today)' | ruby -e 'tasks=STDIN.read.split("\n"); first=tasks.first.split; puts "#{tasks.length} tasks left. Next: ([#{first[1]}] #{first[4]} #{content=first[6..16].join(" "); content.length < 30 ? content : "#{content[0..30]}..."})"'
